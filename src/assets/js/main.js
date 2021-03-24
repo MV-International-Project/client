dfef1e01509cdfb2ffe43fc5b36f3925e07cf5e0
LiamD <contact@liamd.me>
2021-03-24 15:59:22 +0100

    Created matches page

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,36 +5,41 @@ let navigationMgr;
 const init = () => {
     navigationMgr = new Navigation();
     navigationMgr.addPage(new Page("page-discover", {authenticated: false, inNavBar: true}));
-    navigationMgr.addPage(new Page("page-matches", {authenticated: false, inNavBar: true, onOpen: () => {
-        let moveTo = '100';
-        if (navigationMgr._history[navigationMgr._history.length -1].id === 'page-settings') moveTo = '-100';
-
-        document.querySelector('#page-matches').style.transition = 'width .3s ease-in-out';
-        document.querySelector('#page-matches').style.transform = `translateX(${moveTo}%)`;
-        setTimeout(() => {
-            document.querySelector('#page-matches').style.transition = 'transform .3s ease-in-out';
-            document.querySelector('#page-matches').classList.add('show');
-            document.querySelector('#page-matches').style.transform = ``;
-        }, 10);
-        return true;
-    }, onClose: (np) => {
-        let moveTo = '100';
-        if (np === 'page-settings') moveTo = '-100';
-
-        document.querySelector('#page-matches').style.transition = 'transform .3s ease-in-out';
-        document.querySelector('#page-matches').style.transform = `translateX(${moveTo}%)`;
-        setTimeout(() => {
-            document.querySelector('#page-matches').classList.remove('show');
-            setTimeout(() => {
-                document.querySelector('#page-matches').style.transform = `0`;
-                document.querySelector('#page-matches').style.transition = '';
-            }, 300)
-        }, 10);
-        return true;
-    }}));
+    navigationMgr.addPage(new Page("page-matches", {authenticated: false, inNavBar: true, onOpen: openMatches, onClose: closeMatches}));
     navigationMgr.addPage(new Page("page-settings", {authenticated: false, inNavBar: true}));
 
-    navigationMgr.preload('page-discover');
+    navigationMgr.preload('page-matches');
+}
+
+const openMatches = () => {
+    let moveTo = '100';
+    if (navigationMgr._history.length < 1) return true;
+    if (navigationMgr._history[navigationMgr._history.length -1].id === 'page-settings') moveTo = '-100';
+
+    document.querySelector('#page-matches').style.transition = 'width .3s ease-in-out';
+    document.querySelector('#page-matches').style.transform = `translateX(${moveTo}%)`;
+    setTimeout(() => {
+        document.querySelector('#page-matches').style.transition = 'transform .3s ease-in-out';
+        document.querySelector('#page-matches').classList.add('show');
+        document.querySelector('#page-matches').style.transform = ``;
+    }, 10);
+    return true;
+}
+
+const closeMatches = (np) => {
+    let moveTo = '100';
+    if (np === 'page-settings') moveTo = '-100';
+
+    document.querySelector('#page-matches').style.transition = 'transform .3s ease-in-out';
+    document.querySelector('#page-matches').style.transform = `translateX(${moveTo}%)`;
+    setTimeout(() => {
+        document.querySelector('#page-matches').classList.remove('show');
+        setTimeout(() => {
+            document.querySelector('#page-matches').style.transform = `0`;
+            document.querySelector('#page-matches').style.transition = '';
+        }, 300)
+    }, 10);
+    return true;
 }
 
 document.addEventListener("DOMContentLoaded", init);
