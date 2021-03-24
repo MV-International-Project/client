6d6f06c707b30b2c8aa8b18afb08af67aaf12b0e
LiamD <contact@liamd.me>
2021-03-24 10:20:45 +0100

    Added swiping functionality

--- src/assets/js/swipe.js
+++ src/assets/js/swipe.js
@@ -1,28 +1,59 @@
 "use strict";
 
 let swipeInitialized = false;
-let 
+let startPos, lastPos;
 
-const init = () => {
+const swipeInit = () => {
     document.querySelector('#swipe-box').addEventListener('touchstart', startSwipe);
     document.querySelector('#swipe-box').addEventListener('touchmove', swiping);
     document.querySelector('#swipe-box').addEventListener('touchend', endSwipe);
+
+    document.querySelector('#denyUser').addEventListener('click', (e) => {
+        e.preventDefault();
+        cleanupSwipe(false);
+    });
+    document.querySelector('#likeUser').addEventListener('click', (e) => {
+        e.preventDefault();
+        cleanupSwipe(true);
+    });
 }
 
 const startSwipe = (e) => {
-
+    startPos = e.touches[0].clientX;
+    lastPos = 0;
 }
 
 const swiping = (e) => {
-    if (!swipeInitialized) {
+    e.preventDefault();
+    console.log(((e.touches[0].clientX / window.innerWidth) * 12) - 6);
+    lastPos = ((e.touches[0].clientX / window.innerWidth) * 12) - 6;
+    document.querySelector('#swipe-box article.active').style.transform = `rotate(${lastPos}deg) translateY(-500%)`;
+}
 
-
-        swipeInitialized = true;
+const endSwipe = (e) => {
+    if (lastPos < -5) {
+        cleanupSwipe(false)
+    } else if (lastPos > 5) {
+        cleanupSwipe(true)
+    } else {
+        document.querySelector('#swipe-box article.active').style.transition = 'transform .3s ease-in-out';
+        document.querySelector('#swipe-box article.active').style.transform = '';
+        setTimeout(() => document.querySelector('#swipe-box article.active').style.transition = '', 400);
     }
 }
 
-const endSwipe = () => {
+const cleanupSwipe = (accepted) => {
+    const userId = document.querySelector('#swipe-box article.active').id;
+    document.querySelector('#swipe-box article.active').style.transition = 'transform .3s ease-in-out';
+    document.querySelector('#swipe-box article.active').style.transform = `rotate(${accepted ? '': '-'}8deg) translateY(-500%)`;
+    setTimeout(() => {
+        document.querySelector('#swipe-box article.active').style.transition = '';
+        document.querySelector('#swipe-box article.active').remove();
+    }, 400);
 
+    // loadNewUser();
+    // if (accepted) acceptUser(userId);
+    // else denyUser(userId);
 }
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', swipeInit);
