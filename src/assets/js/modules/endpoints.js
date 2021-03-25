389c94360012f2e5f5cf9900740b3d7a4da58833
maarten.vercruysse <maarten.vercruysse@student.howest.be>
2021-03-25 10:36:28 +0100

    Remove redirect since server does it now instead

--- src/assets/js/modules/endpoints.js
+++ src/assets/js/modules/endpoints.js
@@ -4,10 +4,6 @@ import config from "./config.js"
 
 const base = `http://${config.host}:${config.port}/api/`
 
-function constructRedirectURL() {
-    return window.location.origin.replace("localhost", "127.0.0.1") + window.location.pathname.replace("sign.html", "redirect.html")
-}
-
 export function authorizeWithDiscord() {
     location.href = "https://discord.com/api/oauth2/authorize?" + new URLSearchParams({
         redirect_uri: base + "users/login",
@@ -15,15 +11,4 @@ export function authorizeWithDiscord() {
         client_id: config.client_id,
         response_type: "code"
     })
-}
-
-export function getAccessToken(code) {
-    return fetch(base + "users/login",
-        {
-            method: "post",
-            body: {
-                code: code
-            }
-        }
-    )
 }
--- src/assets/js/redirect.js
+++
@@ -1,7 +0,0 @@
-import * as endpoints from './modules/endpoints.js'
-
-
-const urlParams = new URLSearchParams(window.location.search);
-const code = urlParams.get('code');
-
-endpoints.getAccessToken(code);
